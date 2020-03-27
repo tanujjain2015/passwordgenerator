f200227b1d1ce70c0f4b34fee848a1aec95dfe98
tanjain <tanuj.jain@oracle.com>
2020-03-27 12:31:49 -0700

    Modify local environment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
 
+/*  Problem Statement: 
+1. Here are the password Criteria: 
+    a.  Length of password should be between (Min 8 Char and Max 128 Char) 
+    b.  Password could have Uppercase choice (Customer choice)
+    c.  Password could have Lower Case
+    d.  Password could have numbers
+    e.  Password could have special Characters. 
+
+2. Minimum one choice should be selected out of 1b, 1c, 1d and 1e.   
+   If no choice selected then prompt user for re-select the choice.
+
+3. Validate the generated password contains criteria selected by User. 
+
+4. Display password written on the page. 
+*/ 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
